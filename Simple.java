c70245bb5e7cb4b809fdcf76486bcb7ce8efd3e3
Emine Oncu <eoncu@gmail.com>
2022-06-17 10:54:27 +0530

    push java program - auto poll scm

--- Simple.java
+++ Simple.java
@@ -1,5 +1,5 @@
 class Simple{  
     public static void main(String args[]){  
-     System.out.println("First Java Program Integrated with Jenkins");  
+     System.out.println("First Java Program that will auto pull and build using Poll SCM");  
     }  
 }  
